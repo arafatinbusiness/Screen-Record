b78a6884f17df8b5ee9d268d3ef80860e21c13a2
Arafat <arafatinbusiness@gmail.com>
2026-05-20 13:55:24 +0600

    Hide YouTube branding with modestbranding=1

--- frontend/components/video-player.tsx
+++ frontend/components/video-player.tsx
@@ -80,7 +80,7 @@ export function VideoPlayer({ video }: VideoPlayerProps) {
           <iframe
             width="100%"
             height="100%"
-            src={`${embedUrl}?autoplay=1&rel=0`}
+            src={`${embedUrl}?autoplay=1&rel=0&modestbranding=1&iv_load_policy=3`}
             title={video.title}
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
